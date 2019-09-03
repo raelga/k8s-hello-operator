97bdf32b8b4834f2b14bd24205263a8934898984
rael <rael@rael.io>
2019-09-03 23:31:19 +0200

    Deploy a ReplicaSet instead of a Pod

--- pkg/controller/hellohttpservice/hellohttpservice_controller.go
+++ pkg/controller/hellohttpservice/hellohttpservice_controller.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	hellohttpv1alpha1 "github.com/raelga/k8s-hello-operator/pkg/apis/hellohttp/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -77,8 +78,6 @@ type ReconcileHelloHttpService struct {
 
 // Reconcile reads that state of the cluster for a HelloHttpService object and makes changes based on the state read
 // and what is in the HelloHttpService.Spec
-// TODO(user): Modify this Reconcile function to implement your Controller logic.  This example creates
-// a Pod as an example
 // Note:
 // The Controller will requeue the Request to be processed again if the returned error is non-nil or
 // Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
@@ -101,40 +100,52 @@ func (r *ReconcileHelloHttpService) Reconcile(request reconcile.Request) (reconc
 	}
 
 
-	pod := newPodForCR(instance)
+	rs := newReplicaSetCR(instance)
 
 	// Set HelloHttpService instance as the owner and controller
-	if err := controllerutil.SetControllerReference(instance, pod, r.scheme); err != nil {
+	if err := controllerutil.SetControllerReference(instance, rs, r.scheme); err != nil {
 		return reconcile.Result{}, err
 	}
 
 	// Check if this Pod already exists
-	podFound := &corev1.Pod{}
-	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}, podFound)
+	rsFound := &appsv1.ReplicaSet{}
+	err = r.client.Get(context.TODO(), types.NamespacedName{Name: rs.Name, Namespace: rs.Namespace}, rsFound)
 	if err != nil && errors.IsNotFound(err) {
-		reqLogger.Info("Creating a new Pod", "Pod.Namespace", pod.Namespace, "Pod.Name", pod.Name)
-		err = r.client.Create(context.TODO(), pod)
+		reqLogger.Info("Creating a new ReplicaSet", "ReplicaSet.Namespace", rs.Namespace, "ReplicaSet.Name", rs.Name)
+		err = r.client.Create(context.TODO(), rs)
 		if err != nil {
 			return reconcile.Result{}, err
 		}
 
-		// Pod created successfully - don't requeue
+		// ReplicaSet created successfully - don't requeue
 		return reconcile.Result{}, nil
 	} else if err != nil {
 		return reconcile.Result{}, err
 	}
 
-	// Pod already exists - don't requeue
-	reqLogger.Info("Skip reconcile: Pod already exists", "Pod.Namespace", podFound.Namespace, "Pod.Name", podFound.Name)
+	// ReplicaSet already exists - don't requeue
+	reqLogger.Info("Skip reconcile: ReplicaSet already exists", "ReplicaSet.Namespace", rsFound.Namespace, "ReplicaSet.Name", rsFound.Name)
 	return reconcile.Result{}, nil
 }
 
-// newPodForCR returns a busybox pod with the same name/namespace as the cr
-func newPodForCR(cr *hellohttpv1alpha1.HelloHttpService) *corev1.Pod {
+
+// newReplicaSetCR returns a hello-http rs with the same name/namespace as the cr
+func newReplicaSetCR(cr *hellohttpv1alpha1.HelloHttpService) *appsv1.ReplicaSet {
 	labels := map[string]string{
 		"app": cr.Name,
 	}
-	return &corev1.Pod{
+	return &appsv1.ReplicaSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      cr.Name + "-rs",
+			Namespace: cr.Namespace,
+			Labels:    labels,
+		},
+		Spec: appsv1.ReplicaSetSpec{
+			Replicas: cr.Spec.Size,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: labels,
+			},
+			Template: corev1.PodTemplateSpec{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      cr.Name + "-pod",
 			Namespace: cr.Namespace,
@@ -149,6 +160,8 @@ func newPodForCR(cr *hellohttpv1alpha1.HelloHttpService) *corev1.Pod {
 						{Name: "HELLO_NAME", Value: cr.Spec.Subject},
 					},
 				},
+			},
+		},
 			},
 		},
 	}
